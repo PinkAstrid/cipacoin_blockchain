a9be70a93192fd66640c2fc838241e6630f7a5e4
Marie-Astrid Chanteloup <marie-astrid.chanteloup@telecomnancy.eu>
2021-03-19 08:23:37 +0100

    le bon dossier

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,87 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.25 <0.7.0;
+pragma solidity >=0.4.22 <0.9.0;
 
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
+contract CipaCoins{
+  
+  struct Club{
+    bytes32 name;
+    address pres;
+    uint cipaClubBalance;
   }
 
-  constructor() public {
-    owner = msg.sender;
+  struct Eleve{
+    address eleve;
+    uint cipaStudentBalance;
+    bool certificat;
   }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+  address public directionDesEtudes;
+
+  uint cipaThreashHold;
+
+  mapping(address => Eleve) public eleves;
+
+  Club[] public clubs;
+
+  function sendCipaStudentToStudent(address student, uint amount) public {
+    require(
+      eleves[msg.sender].cipaStudentBalance>amount,
+      "L'eleve envoyeur n'a pas assez de CIPA."
+    );
+
+    require(
+      !eleves[student].certificat,
+      "L'eleve receveur a deja assez de CIPA."
+    );
+    
+    eleves[student].cipaStudentBalance+=amount;
+    eleves[msg.sender].cipaStudentBalance-=amount;
   }
+
+  function sendCipaDirToClub(uint amount, uint clubInt) public {
+    require(
+      msg.sender== directionDesEtudes,
+      "Seule la direction des etudes peut creer des CIPA."
+    );
+
+    require(
+      clubInt>=clubs.length,
+      "Le club n'est pas connu."
+    ); 
+    
+    clubs[clubInt].cipaClubBalance+=amount;
+  }
+
+  function sendCipaClubToStudent(address student, uint amount, uint clubInt) public {
+    require(
+      clubInt>=clubs.length,
+      "Le club n'est pas connu."
+    );
+
+    require(
+      msg.sender==clubs[clubInt].pres,
+      "L'envoyeur n'est pas president du club."
+    );
+    
+    require(
+      clubs[clubInt].cipaClubBalance>amount,
+      "Le club envoyeur n'a pas assez de CIPA."
+    );
+
+    require(
+      !eleves[student].certificat,
+      "L'eleve receveur a deja assez de CIPA."
+    );
+    
+    eleves[student].cipaStudentBalance+=amount;
+    clubs[clubInt].cipaClubBalance-=amount;
+  }
+
+  function createClub(address president, bytes32 name) public {
+    require(
+      msg.sender== directionDesEtudes,
+      "Seule la direction des etudes peut creer des clubs"
+    );
+  }
+
 }
