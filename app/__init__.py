ea41df7d6b489e30eba176c9ebc8e891aa60fe8a
Ambroise Sander <ambroise.sander@telecomnancy.eu>
2021-04-28 23:09:21 +0200

    ajout fichier config

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
 import json
+from configparser import ConfigParser
 
 from flask import Flask
 from web3 import Web3
-from web3.contract import ConciseContract
 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-with open("build/contracts/CipaCoin.json") as f:
+config = ConfigParser()
+config.read("config.cipa")
+
+chain_url = config.get('chaine', 'url', fallback="http://127.0.0.1")
+chain_port = config.get('chaine', 'port', fallback="7545")
+abi_path = config.get('contrat', 'chemin_abi', fallback="build/contracts/CipaCoin.json")
+contract_address = config.get('contrat', 'adresse', fallback=None)
+
+if contract_address is None:
+    raise ValueError("Aucune adresse de contrat n'a été fournie, merci de vérifier le contenu de 'config.cipa'")
+
+w3 = Web3(Web3.HTTPProvider(f"{chain_url}:{chain_port}"))
+with open(abi_path) as f:
     abi = json.load(f)["abi"]
 
-address = "0x468973774Ce61Af965Ff10919b0133cBd1Fc1539"
-contract = w3.eth.contract(address=address, abi=abi)
+contract = w3.eth.contract(address=contract_address, abi=abi)
 accounts = w3.eth.accounts
 
 app = Flask(__name__)
